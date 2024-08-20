6f0f2eef58858f36e44b6571cd34495f3ee56065
drazka <izabdr@gmail.com>
2024-08-20 22:22:39 +0200

    task3 corrected

--- js/task-3.js
+++ js/task-3.js
@@ -2,11 +2,10 @@ const input = document.querySelector("input#name-input");
 const output = document.querySelector("span#name-output");
 
 input.addEventListener("input", (event) => {
-    const inputValue = event.currentTarget.value;
-    if (inputValue.trim() === "") {
-        return; // Nic nie rób, jeśli wpisano tylko biały znak
+    if (event.currentTarget.value.trim() === "") {
+        return;
       } else {
-        output.textContent = inputValue;
+        output.textContent = event.currentTarget.value.trim();
       }
         }
   );
